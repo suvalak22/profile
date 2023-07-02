2e0abcbdcd5b1d412cc7ef28d271ed4e1735a849
suvalak22 <suvalak.na.65@ubu.ac.th>
2023-07-02 22:26:08 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
+import './styles.css';
 
-function App() {
-  return (
+function App(){
+  return(
     <>
-      <div className = 'card'>
+      <div className='card'>
+        <Avatar />
         <div className='data'>
-
+          <Intro />
+          <SkillList />
         </div>
       </div>
     </>
-  )
+  );
 }
 
-function Avatar() {
-  return <img className='avatar' scr="IMG_3104.jpg" alt='My Avatar'/>;
+function SkillList(){
+  return(
+    <div className='skill-list'>
+      <Skill skill='sing' emoji='🎤🤏' color='orange' />
+      <Skill skill='photograph' emoji='📸' color='skyblue' />
+      <Skill skill='python' emoji='👍' color='yellow' />
+      <Skill skill='javascript' emoji='👊' color='lime' /> 
+    </div>
+  );
+}
+
+function Skill(props){
+  return(
+    <div className='skill' style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Intro(){
+  return(
+    <div>
+      <h1>นางสาวสุวลักษณ์ นามแสง</h1>
+      <p>
+        นักศึกษาสาวict สาวสวยสายฮา น่ารัก อารมณ์ดี เฟรนลี่ จากสาขาเทคโนโลยีสารสนเทศและการสื่อสาร คณะวิทยาศาสตร์
+        มหาวิทยาลัยอุบลราชธานี ชื่นชอบการถ่ายรูปร้องเพลง และอยากเป็นนักพัฒนาเว็บที่เก่งๆในอนาคต
+      </p>
+    </div>
+  );
+}
+
+function Avatar(){
+  return <img className='avatar' src='IMG_3810.jpg' alt="My Avatar" ></img>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -24,4 +58,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
